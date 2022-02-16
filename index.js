d26c193fe0f090c1f9b795f77e9e13502073ac9a
tnwks_77 <tnwks_77@naver.com>
2022-02-17 00:04:36 +0900

    heroku 배포준비

--- index.js
+++ index.js
@@ -1,26 +1,151 @@
-var http = require("http");
-var hostname = "127.0.0.1";
-var port = 8080;
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const models = require("./models");
+const multer = require("multer");
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads/");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
+});
+const port = process.env.PORT || 8080;
 
-const server = http.createServer(function (req, res) {
-  const path = req.url;
-  const method = req.method;
-  if (path === "/products") {
-    if (method === "GET") {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      const products = JSON.stringify([
-        {
-          name: "농구공",
-          price: 5000,
-        },
-      ]);
-      res.end(products);
-    } else if (method === "POST") {
-      res.end("생성되었습니다");
-    }
-  }
-  res.end("good bye");
+app.use(express.json());
+app.use(cors());
+app.use("/uploads", express.static("uploads"));
+
+//배너를 주는 API
+app.get("/banners", (req, res) => {
+  models.Banner.findAll({
+    limit: 2,
+  })
+    .then((result) => {
+      res.send({
+        banners: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("에러가발생했습니다");
+    });
+});
+//조회
+app.get("/products", (req, res) => {
+  models.Product.findAll({
+    //정렬
+    order: [["createdAt", "DESC"]],
+    //어떤 컬럼을 가져올거냐 설정(필요한 데이터만 가져오게)
+    attributes: [
+      "id",
+      "name",
+      "price",
+      "createdAt",
+      "seller",
+      "imageUrl",
+      "soldout",
+    ],
+  })
+    .then((result) => {
+      console.log("PRODUCTS:", result);
+      res.send({
+        products: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send("에러발생");
+    });
 });
 
-server.listen(port, hostname);
-console.log("grab amrket server on!");
+app.post("/products", (req, res) => {
+  const body = req.body;
+  const { name, description, price, seller, imageUrl } = body;
+  if (!name || !description || !price || !seller || !imageUrl) {
+    res.status(400).send("모든 필드를 입력해주세요");
+  } //방어코드(데이터베이스에 직접적으로 영행을 주면 코드에 문제가 없게)
+  models.Product.create({
+    name,
+    description,
+    price,
+    seller,
+    imageUrl,
+  })
+    .then((result) => {
+      console.log("상품 생성 결과:", result);
+      res.send({ result });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send("상품 업로드에 문제가 발생했습니다.");
+    });
+});
+
+app.get("/products/:id", (req, res) => {
+  const params = req.params;
+  const { id } = params;
+  models.Product.findOne({
+    where: {
+      id: id,
+    },
+  })
+    .then((result) => {
+      console.log("PRODUCT:", result);
+      res.send({
+        product: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send("상품 조회에 에러가 발생했습니다.");
+    });
+});
+
+app.post("/image", upload.single("image"), (req, res) => {
+  const file = req.file;
+  console.log(file);
+  res.send({
+    imageUrl: file.path,
+  });
+});
+
+app.post("/purchase/:id", (req, res) => {
+  const { id } = req.params;
+  models.Product.update(
+    {
+      soldout: 1,
+    },
+    {
+      where: {
+        id,
+      },
+    }
+  )
+    .then((result) => {
+      res.send({
+        result: true,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("에러가 발생했습니다");
+    });
+});
+
+app.listen(port, () => {
+  console.log("감쟈의 서버가 잘 돌아가고 있습니다.");
+  models.sequelize
+    .sync()
+    .then(() => {
+      console.log("✓ DB 연결 성공");
+    })
+    .catch(function (err) {
+      console.error(err);
+      console.log("✗ DB 연결 에러");
+      process.exit();
+    });
+});
